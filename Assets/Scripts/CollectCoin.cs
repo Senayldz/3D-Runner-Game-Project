e004ad7bdeca7f77287f1df8f139c80eb97f70ca
Senayldz <senasenayildiz@gmail.com>
2023-12-22 17:11:29 +0300

    Added obstacles, obstacles animations, character win, lose animations and fixed design

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollectCoin : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI coinText;
+    public PlayerController playerController;
+    public int maxScore;
+    public Animator PlayerAnim;
+    public GameObject Player;
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        PlayerAnim = Player.GetComponentInChildren<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             AddCoin();
             Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("End"))
+        {
+            playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+            if(score >= maxScore)
+            {
+                //Debug.Log("You win");
+                PlayerAnim.SetBool("win", true);
+            }
+            else
+            {
+                //Debug.Log("You lose");
+                PlayerAnim.SetBool("lose", true);
+            }
         }
     }
 
@@ -21,6 +46,7 @@ public class CollectCoin : MonoBehaviour
         if(collision.collider.CompareTag("Collision"))
         {
             Debug.Log("Engele dokundu");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
